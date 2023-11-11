069c79472911a138c50dce51d238359a5ec8b16e
Jakup Emini <jakupknio@gmail.com>
2023-11-11 17:34:50 +0100

    finished 02

---
+++ 03/01-starting-setup/src/components/Expenses/Expenses.js
@@ -0,0 +1,19 @@
+import './Expenses.css'
+import ExpenseItem from './ExpenseItem'
+import Card from '../UI/Card'
+
+function Expenses(props) {
+    return (
+        <Card className='expenses'>
+            {props.expenses.map(x => 
+                <ExpenseItem 
+                amount={x.amount}
+                title={x.title}
+                date={x.date}
+                ></ExpenseItem>
+            )}
+        </Card>
+    );
+}
+
+export default Expenses;
